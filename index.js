25f93f76106ff7683cc5506960bab01d05804f7a
Derrick Schneider <derrick.schneider@gmail.com>
2012-09-23 14:01:29 -0700

    Initial support for retrieving values from the metricsmaw server.
    - util.js
      Add decode methods for dealing with erlang binaries
    
    - metricsmaw.js
      Add an export for retrieving the list of metric names in the server
         - Opens a socket, sends the data, waits on the response, closes socket
    
      Revamp internals to use on("connect") for socket handling
    
    - index.js
      Add a timer method that gets the list of metrics and prints them out

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var net = require('net')
 
 setInterval(
     function() {
-        metrics.init("127.0.0.1",18000)
+        metrics.init("127.0.0.1",18000);
         metrics.incrementCounter('node_tests',25);
     },10000);
+    
+setInterval(function() {
+     metrics.init("127.0.0.1",18000);
+     var metricNames = metrics.getAllMetricNames(function(err,results) {console.log(results);});
+},5000);
